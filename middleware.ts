f9f64c835a318e5833128f6b04dfbc8519946842
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-11-29 11:08:03 -0300

    Create middleware to handle redirect and auth

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+function handleRootRedirect(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/todos', request.url))
+  }
+}
+
+function handleAuth(request: NextRequest) {
+  const isAuthenticated = request.cookies.get('auth-token')?.value
+
+  if (!isAuthenticated) {
+    const loginUrl = new URL('/login', request.url)
+    loginUrl.searchParams.set('from', request.nextUrl.pathname)
+
+    return NextResponse.redirect(loginUrl)
+  }
+
+  return NextResponse.next()
+}
+
+export function middleware(request: NextRequest) {
+  return (
+    handleRootRedirect(request) || handleAuth(request) || NextResponse.next()
+  )
+}
+
+export const config = {
+  matcher: ['/', '/todos/:path*'],
+}

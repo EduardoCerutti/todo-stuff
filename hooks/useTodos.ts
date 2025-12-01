b39045a16f0f6a800e1c8860606dd959c991914c
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-12-01 16:29:19 -0300

    Make useTodos work with sessionStorage

--- hooks/useTodos.ts
+++ hooks/useTodos.ts
@@ -1,9 +1,21 @@
 import { getTodos } from '@/lib/api/getTodos'
 import { useQuery } from '@tanstack/react-query'
+import { getTodosCache, setTodosCache } from '@/lib/storage'
 
 export function useTodos({ skip = 0, limit = 10 }) {
   return useQuery({
     queryKey: ['todos', skip, limit],
-    queryFn: () => getTodos({ skip, limit }),
+    queryFn: async () => {
+      const cachedData = getTodosCache(skip, limit)
+      if (cachedData) {
+        return cachedData
+      }
+
+      const data = await getTodos({ skip, limit })
+
+      setTodosCache(skip, limit, data)
+
+      return data
+    },
   })
 }
